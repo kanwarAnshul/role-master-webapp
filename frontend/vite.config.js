b086275e667bc9177a1f5488964b8db226729dd1
arpit kanwar <168622732+kanwarAnshul@users.noreply.github.com>
2024-11-25 11:26:39 +0530

    Update vite.config.js
    add deploy backend in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:7000',  
+      '/api': 'https://role-master-webapp-backend.onrender.com',  
     },
   },
   plugins: [react()],
